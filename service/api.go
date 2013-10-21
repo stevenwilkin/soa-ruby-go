d0169b6092a161ee1e8f3910cd8d1c4483d5e55f
Steven Wilkin <stevenwilkin@gmail.com>
2013-10-21 23:42:53 +0100

    Store a new item using body of POST

--- service/api.go
+++ service/api.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/pilu/traffic"
+	"io/ioutil"
 	"net/http"
+)
+
+type Item struct {
+	Id   uint
+	Text string
+}
+
+var (
+	items       = map[uint]string{}
+	nextId uint = 1
 )
 
 func itemsHandler(w traffic.ResponseWriter, r *http.Request) {
 }
 
 func createItemHandler(w traffic.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+
+	id := nextId
+	items[id] = string(body)
+	nextId++
+
+	item := Item{id, string(body)}
+	b, _ := json.Marshal(item)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
 
 func itemHandler(w traffic.ResponseWriter, r *http.Request) {
