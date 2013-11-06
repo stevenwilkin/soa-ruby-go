b62bf063ecd6bf266e592b5de85a974afa6acf1e
Steven Wilkin <stevenwilkin@gmail.com>
2013-11-06 22:56:20 +0000

    Rejig demo script

--- demo.rb
+++ demo.rb
@@ -7,15 +7,22 @@ require 'item'
 
 Item.base_url = 'http://0.0.0.0:7000/items'
 
-puts 'Creating an item:'
-puts Item.create 'First!'
-
-puts
-puts 'A single item:'
-puts Item.find(2)
-
-puts
-puts 'All items:'
-Item.all.each do |item|
-  puts item
+def list_items
+  puts
+  puts '> List items:'
+  Item.all.each do |item|
+    puts item
+  end
 end
+
+puts '> Create items:'
+%w{first second third forth}.each do |text|
+  puts text
+  Item.create text
+end
+
+list_items
+
+puts
+puts '> Retrieve item 2:'
+puts Item.find(2)
